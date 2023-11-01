abaa4ccbeea111b38b45de6e94d782894f704831
Danilo <105374584+DaniloDuque@users.noreply.github.com>
2023-11-01 11:52:59 -0600

    Create TopologicalSort.cpp

---
+++ TopologicalSort/TopologicalSort.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <bitset>
+using namespace std;
+
+#define msk bitset<100001>
+#define vi vector<int>
+#define vvi vector<vi>
+
+
+
+
+void topSortAux(vvi &graph, vi &topSort, msk &vis, int start){
+
+    if(vis.test(start)) return;
+    vis.set(start);
+    for(int neigh: graph[start]){
+
+        if(!vis.test(neigh)) topSortAux(graph, topSort, vis, neigh);
+
+    }
+    topSort.insert(topSort.begin(), start);
+
+}
+
+
+
+
+vi TopologicalSort(vvi &graph){
+
+    vi topSort;
+    msk vis(0);
+    for(int i = 0; i<graph.size(); i++)
+
+        if(!vis.test(i)) topSortAux(graph, topSort, vis, i); 
+
+    return topSort;
+
+}
+
+
+
+
+
+
+
+
+int main(){
+
+    int n, m, node1, node2;
+    cin>>n>>m;
+    vvi graph(n);
+    msk vis(0);
+
+    for(int i = 0; i<m; i++){
+
+        cin>>node1>>node2;
+        graph[node1].push_back(node2);
+
+    }
+
+    vi topSort = TopologicalSort(graph);
+    for(int i = 0; i<topSort.size(); i++) cout<<topSort[i]<<" ";
+    cout<<endl;
+
+    return 0;
+}
+
+
