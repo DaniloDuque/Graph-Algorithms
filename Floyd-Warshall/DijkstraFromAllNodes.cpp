695b01732f7904bd30d80096ff2d6753c7ac9da4
Danilo <105374584+DaniloDuque@users.noreply.github.com>
2023-11-08 14:49:21 -0600

    Update DijkstraFromAllNodes.cpp

--- Floyd-Warshall/DijkstraFromAllNodes.cpp
+++ Floyd-Warshall/DijkstraFromAllNodes.cpp
@@ -1,80 +1,63 @@
-#include <iostream>
-#include <queue>
-#include <vector>
-#include <bitset>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define msk bitset<100001>
+#define msk bitset<5000>
 #define duo pair<int, int>
 #define vvi vector<vector<duo>>
-#define minHeap priority_queue<duo, vector<duo>, compare>
+#define minHeap priority_queue<int, vector<int>, compare>
 
-
+vector<int> memo;
 
 struct compare{
 
-    bool operator()(const duo &p1, const duo &p2){ return p1.second > p2.second; } 
+    bool operator()(const int &p1, const int &p2){ return memo[p1] > memo[p2]; } 
 
 };
 
 
 
 
+vector<int> dijkstra(vvi &graph, int i){
 
-int * makeArr(int n){
-
-    int * minRoutes = (int*)malloc(n*sizeof(int));
-    for(int i = 0; i<n; i++) minRoutes[i] = 1<<30;
-    return minRoutes;
-    
-}
-
-
-
-
-
-
-int * dijkstra(vvi &graph, int i){
-
-    int * minRoutes = makeArr(graph.size()); minRoutes[i] = 0;
-    minHeap pq; pq.push({i, 0});
+    memo.assign(graph.size(), 1<<30); memo[i] = 0;
+    minHeap pq; pq.push(i);
     msk vis(0); 
 
     while(!pq.empty()){
 
-        int curr = pq.top().first;
+        int curr = pq.top();
         pq.pop();  
         vis.set(curr); 
 
-        if(vis.count() == graph.size()) return minRoutes; // if we visited all nodes, return routes
+        if(vis.count() == graph.size()) return memo; // if we visited all nodes, return routes
         
         for(duo neigh: graph[curr])
 
-            if(!vis.test(neigh.first) && minRoutes[neigh.first] > minRoutes[curr] + neigh.second)
+            if(!vis.test(neigh.first) && memo[neigh.first] > memo[curr] + neigh.second){
 
-                pq.push({neigh.first, minRoutes[neigh.first] = minRoutes[curr] + neigh.second});
+                memo[neigh.first] = memo[curr] + neigh.second;
+                pq.push(neigh.first);
 
+            }
 
-    }return minRoutes;
+    }return memo;
     
 }
 
 
-//O(n*n*logn)
 
 int main(){
 
     int n, m, node1, node2, weight;
-    cin>>n>>m;
+    cin>>n>>m; // n->amount of vertices    m->amount of edges
     vvi graph(n);
     for(int i = 0; i<m; i++){
 
-        cin>>node1>>node2>>weight;
+        cin>>node1>>node2>>weight;       //from node1 to node2 with weight cost
         graph[node1].push_back({node2, weight});
 
     }
-
-    int ** minRoutes = (int**)malloc(n*sizeof(int*));
+    vector<vector<int>> minRoutes(n);
     for(int i = 0; i<n; i++) minRoutes[i] = dijkstra(graph, i);
 
     for(int i = 0; i<n; ++i){
@@ -83,6 +66,3 @@ int main(){
     }
 
 }
-
-
-
