cc8db890ceec715abd02f330c27d3f73e108edd7
Danilo <105374584+DaniloDuque@users.noreply.github.com>
2023-11-20 23:51:30 -0600

    Update Hierholzer.cpp

--- Hierholzer/Hierholzer.cpp
+++ Hierholzer/Hierholzer.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 using namespace std;
 
 #define pv pair<vector<int>, vector<int>>
@@ -36,7 +37,7 @@ bool hasEulerianPath(int size, vi &in_Deg, vi &out_Deg){
 
         else if(in_Deg[i] - out_Deg[i] == 1) endV++;    //if in > out, then we found a possible end vertex;
 
-    }return (!strtV && !endV) || (strtV == endV == 1);  //if there is one end vertex, then there has to be one and only one start vertex
+    }return (!strtV && !endV) || (strtV == 1 && endV == 1);  //if there is one end vertex, then there has to be one and only one start vertex
 
 }
 
@@ -60,7 +61,7 @@ int startVertex(vvi &graph, vi &in_Deg, vi &out_Deg){
 
 
 
-vi path;
+stack<int> path;
 
 
 void dfs(vvi &graph, vi &out_Deg, int curr){
@@ -71,14 +72,14 @@ void dfs(vvi &graph, vi &out_Deg, int curr){
         --out_Deg[curr]; graph[curr].pop_back(); //we will visit vertex nxt, so, eliminate the edge from curr to nxt
         dfs(graph, out_Deg, nxt);
 
-    }path.insert(path.begin(), curr);
+    }path.push(curr);
 
 }
 
 
 
 
-vi findEulerianPath(vvi &graph){
+stack<int> findEulerianPath(vvi &graph){
 
     pv Degrees = countDegrees(graph);
     if(!hasEulerianPath(graph.size(), Degrees.first, Degrees.second)) return {};
@@ -100,10 +101,13 @@ int main(){
         cin>>v1>>v2;   //edge v1->v2
         graph[v1].push_back(v2);
 
-    }
-    for(int i: findEulerianPath(graph)) cout<<i<<" ";
-    cout<<endl;
+    }stack<int> eulerian = findEulerianPath(graph);
+    
+    while(!eulerian.empty()){
+
+        cout<<eulerian.top()<<" ";
+        eulerian.pop();
+        
+    }cout<<endl;
     return 0;
 }
-
-
